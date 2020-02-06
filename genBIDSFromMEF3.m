f562cc69c988ab8dafc765fcb6d85c2d21eee04e
van den Boom <m218483@r5194199.mayo.edu>
2020-02-06 11:39:12 -0600

    Added JSON output

--- genBIDSFromMEF3.m
+++ genBIDSFromMEF3.m
@@ -1,5 +1,5 @@
 % 
-%   Generate BIDS ieeg output based on a MEF3 dataset
+%   Generate BIDS iEEG amplifier metadata files based on a MEF3 dataset
 %
 %   genBIDSFromMEF3(inputMef, outputDir)
 %
@@ -67,7 +67,7 @@ function genBIDSFromMEF3(inputMef, outputDir)
 
     
     %
-    % build a new channel struct with the BIDS output
+    % build a channel struct with the BIDS output
     %
     
     channels = [];
@@ -96,14 +96,14 @@ function genBIDSFromMEF3(inputMef, outputDir)
             
             % cutoffs
             if ~isempty(section2.low_frequency_filter_setting) &&  section2.low_frequency_filter_setting > 0
-                channels(counter).low_cutoff             = section2.low_frequency_filter_setting;
+                channels(counter).low_cutoff        = section2.low_frequency_filter_setting;
             else
-                channels(counter).low_cutoff             = 'n/a';                                       % TODO: n/a instead of 0? (https://github.com/bids-standard/bids-starter-kit/blob/master/matlabCode/createBIDS_ieeg_channels_tsv.m), leave out, Ask dora
+                channels(counter).low_cutoff        = 'n/a';                                       % TODO: n/a instead of 0? (https://github.com/bids-standard/bids-starter-kit/blob/master/matlabCode/createBIDS_ieeg_channels_tsv.m), leave out, Ask dora
             end
             if ~isempty(section2.high_frequency_filter_setting) &&  section2.high_frequency_filter_setting > 0
-                channels(counter).high_cutoff             = section2.high_frequency_filter_setting;
+                channels(counter).high_cutoff       = section2.high_frequency_filter_setting;
             else
-                channels(counter).high_cutoff             = 'n/a';                                       % TODO: n/a instead of 0? (https://github.com/bids-standard/bids-starter-kit/blob/master/matlabCode/createBIDS_ieeg_channels_tsv.m), leave out, Ask dora
+                channels(counter).high_cutoff       = 'n/a';                                       % TODO: n/a instead of 0? (https://github.com/bids-standard/bids-starter-kit/blob/master/matlabCode/createBIDS_ieeg_channels_tsv.m), leave out, Ask dora
             end
             
             % reference
@@ -129,9 +129,9 @@ function genBIDSFromMEF3(inputMef, outputDir)
             
             % notch
             if ~isempty(section2.notch_filter_frequency_setting) && section2.notch_filter_frequency_setting > 0
-                channels(counter).notch                 = section2.notch_filter_frequency_setting;
+                channels(counter).notch             = section2.notch_filter_frequency_setting;
             else 
-                channels(counter).notch                 = 'n/a';                                    % TODO: n/a instead of 0? (https://github.com/bids-standard/bids-starter-kit/blob/master/matlabCode/createBIDS_ieeg_channels_tsv.m), leave out, Ask dora
+                channels(counter).notch             = 'n/a';                                    % TODO: n/a instead of 0? (https://github.com/bids-standard/bids-starter-kit/blob/master/matlabCode/createBIDS_ieeg_channels_tsv.m), leave out, Ask dora
             end
             
             % status
@@ -148,11 +148,122 @@ function genBIDSFromMEF3(inputMef, outputDir)
     
     
     %
-    % write the channel struct to a tab-seperated file (.tsv)
+    % build a ieeg struct with the BIDS output
     %
     
+    ieeg = [];
+    ieeg.TaskName = '';
+    ieeg.SamplingFrequency = inputMef.time_series_metadata.section_2.sampling_frequency;
+    ieeg.PowerLineFrequency = '';
+    ieeg.SoftwareFilters = '';
+    ieeg.DCOffsetCorrection = '';
+    ieeg.HardwareFilters = [];
+    ieeg.HardwareFilters.HighpassFilter = [];
+    ieeg.HardwareFilters.HighpassFilter.CutoffFrequency = inputMef.time_series_metadata.section_2.high_frequency_filter_setting;
+    ieeg.HardwareFilters.LowpassFilter = [];
+    ieeg.HardwareFilters.LowpassFilter.CutoffFrequency = inputMef.time_series_metadata.section_2.low_frequency_filter_setting;
+    ieeg.Manufacturer = '';
+    ieeg.ManufacturersModelName = '';
+    ieeg.TaskDescription = '';
+    ieeg.Instructions = '';
+    ieeg.CogAtlasID = '';
+    ieeg.CogPOID = '';
+    ieeg.InstitutionName = '';
+    ieeg.InstitutionAddress = '';
+    ieeg.DeviceSerialNumber = '';
+    ieeg.ECOGChannelCount = 0;
+    ieeg.SEEGChannelCount = 0;
+    ieeg.EEGChannelCount = 0;
+    ieeg.EOGChannelCount = 0;
+    ieeg.ECGChannelCount = 0;
+    ieeg.EMGChannelCount = 0;
+    ieeg.MiscChannelCount = 0;
+    ieeg.TriggerChannelCount = 0;
+    ieeg.RecordingDuration = '';                        % TODO: ask dora, which units?
+    ieeg.RecordingType = 'continuous';
+    ieeg.EpochLength = 0;
+    ieeg.SubjectArtefactDescription = '';
+    ieeg.SoftwareVersions = '';
+    ieeg.iEEGReference = '';
+    ieeg.ElectrodeManufacturer = '';
+    ieeg.ElectrodeManufacturersModelName = '';
+    ieeg.iEEGGround = '';
+    ieeg.iEEGPlacementScheme = '';
+    ieeg.iEEGElectrodeGroups = '';
+    ieeg.ElectricalStimulation = '';
+    ieeg.ElectricalStimulationParameters = '';
+    
+    %
+    % write the channel struct to a tab-seperated file (_channels.tsv)
+    %
     writetable( struct2table(channels), ...
                 [outputDir, filesep, datestr(now, 'yyyymmdd_HHMMSS_FFF'), '_channels.tsv'], ...
                 'FileType', 'text', 'Delimiter', '\t');
     
+    %
+    % write the ieeg struct to a JSON file (_ieeg.json)
+    %
+    fileID = fopen([outputDir, filesep, datestr(now, 'yyyymmdd_HHMMSS_FFF'), '_ieeg.json'], 'w');
+    writeElement(fileID, ieeg, '');
+    fprintf(fileID,'\n');
+    fclose(fileID);
+
+end
+
+
+%%%
+%
+% Simple JSON write functions
+% 
+% Needed since matlab's jsonencode function does not provide any indentation or linebreaks to make it 
+% human readable (which is exactly one of the advantages of using JSON)
+% 
+% Code adapted from:
+% Lior Kirsch (2020). Structure to JSON (https://www.mathworks.com/matlabcentral/fileexchange/50965-structure-to-json), MATLAB Central File Exchange. Retrieved February 6, 2020.
+% 
+%%%
+
+function writeElement(fid, data, tabs)
+    namesOfFields = fieldnames(data);
+    numFields = length(namesOfFields);
+    tabs = sprintf('%s\t', tabs);
+    fprintf(fid,'{\n%s', tabs);
+   
+    for i = 1:numFields - 1
+        currentField = namesOfFields{i};
+        currentElementValue = data.(currentField);
+        writeSingleElement(fid, currentField, currentElementValue, tabs);
+        fprintf(fid,',\n%s', tabs);
+    end
+    if isempty(i)
+        i = 1;
+    else
+        i = i + 1;
+    end
+    
+    currentField = namesOfFields{i};
+    currentElementValue = data.(currentField);
+    writeSingleElement(fid, currentField, currentElementValue,tabs);
+    
+    % remove tab
+    if length(tabs) > 0,    tabs = tabs(1:end-1);   end
+    
+    fprintf(fid,'\n%s}',tabs);
+end
+
+function writeSingleElement(fid, currentField,currentElementValue,tabs)
+    if length(currentElementValue) > 1 && ~ischar(currentElementValue)
+        fprintf(fid,'"%s": %s' , currentField, jsonencode(currentElementValue));
+    elseif isstruct(currentElementValue)
+        fprintf(fid,'"%s": ',currentField);
+        writeElement(fid, currentElementValue,tabs);
+    elseif isnumeric(currentElementValue)
+        fprintf(fid,'"%s": %g' , currentField, currentElementValue);
+    elseif ischar(currentElementValue)
+        fprintf(fid,'"%s": "%s"' , currentField, currentElementValue);
+    elseif isempty(currentElementValue)
+        fprintf(fid,'"%s": ""' , currentField);
+    else
+        fprintf(fid,'"%s": "%s"' , currentField, currentElementValue);
+    end
 end
